a80d5669762c6d74bc705e02a77a67391deeda6c
Matt Sharland <matt@mattsharland.com>
2026-07-27 20:01:14 -0400

    Mobile

---
+++ SreAutomationToolkit.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SreAutomationToolkit.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
